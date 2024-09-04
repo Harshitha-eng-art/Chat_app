eb6cb4405fde9ac2a91c124ae04cc4686e0cad44
unknown <harshithatu302003@gmail.com>
2024-09-04 21:13:23 +0530

    second commit

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
-};
-
-export default connectToMongoDB;
+export const connection=()=>
+{
+    mongoose.connect(process.env.MONGO_URI,{
+        dbName:"Chat_application_new"
+    }).then(()=>
+    {
+        console.log("Connected to database")
+    }).catch(err=>
+    {
+        console.log(`connection falid${err}`)
+    })
+}
+export default connection
